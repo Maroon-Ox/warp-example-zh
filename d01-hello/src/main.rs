d51e0b506c2b8e63e8f17bfa5703a14a620e8cfb
xue35@rpi400 <xue@live.it>
2020-12-21 20:54:25 +0800

    Fix tokio version error, and generate doc.

--- d01-hello/src/main.rs
+++ d01-hello/src/main.rs
@@ -1,8 +1,25 @@
-#![deny(warnings)]
+#![doc(html_root_url = "https://warp-in-21-days.rs/")]
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![cfg_attr(test, deny(warnings))]
+//! # Hello.rs
+//!
+//! 使用Warp编写的Hello world网页应用。
+//!
+//! 为了使用到Warp，除了在Cargo.toml中声明。在代码里面还需要明确使用Warp的Filter模块。代码如下：
+//! ``use warp::Filter``
+//!
+//!
+//! tokio是一个Rust编程语言的异步运行时,提供异步事件驱动平台，构建快速，
+//! 可靠和轻量级网络应用。利用Rust的所有权和并发模型确保线程安全。这里我们通过调用``tikio::main``来
+//! 让Warp具备异步处理能力。代码如下：
+//! ``#[tokio::main]``
+//!
+//! 下面我们进入到main函数的编写。
 use warp::Filter;
 
 #[tokio::main]
-pub async fn main() {
+async fn main() {
     // Match any request and return hello world!
     let routes = warp::any().map(|| "Hello, World!");
 
